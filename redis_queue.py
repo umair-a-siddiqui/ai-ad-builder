68322f16853f21b46c23f61cd2d0b4c6a25afd61
Umair Ahmed Siddiqui <prof.umair.siddiqui@gmail.com>
2026-09-10 04:43:13 +0500

    Use Redis URL for deployment

--- redis_queue.py
+++ redis_queue.py
@@ -1,11 +1,20 @@
+import os
 import redis
 from rq import Queue
 
-redis_connection = redis.Redis(
-    host="172.31.203.111",
-    port=6379,
-    decode_responses=False
-)
+redis_url = os.getenv("REDIS_URL")
+
+if redis_url:
+    redis_connection = redis.from_url(
+        redis_url,
+        decode_responses=False
+    )
+else:
+    redis_connection = redis.Redis(
+        host="172.31.203.111",
+        port=6379,
+        decode_responses=False
+    )
 
 video_queue = Queue(
     "video-generation",
