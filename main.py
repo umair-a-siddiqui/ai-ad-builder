058b976c1f6439c62bcb344452959bf35b37063d
Umair Ahmed Siddiqui <prof.umair.siddiqui@gmail.com>
2026-09-10 05:38:25 +0500

    Allow Vercel frontend CORS

--- main.py
+++ main.py
@@ -70,11 +70,12 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "https://adnova-30adwbvik-umair-projects2.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
